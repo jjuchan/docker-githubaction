140b2b0e11ca9aa39c31f21a58cce09f902db7cb
jjuchan <yjc7241@naver.com>
2025-10-24 11:45:46 +0900

    feat: deploy.yml add

---
+++ src/main/java/com/example/kpi024/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.kpi024;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to KPI-024 Application!";
+    }
+}
