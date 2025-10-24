4cc9fc730a65f2e0f59e253747ee02e65a2f6fdc
jjuchan <yjc7241@naver.com>
2025-10-24 11:41:50 +0900

    feat: docker add

---
+++ src/main/java/com/example/kpi024/KpI024Application.java
@@ -0,0 +1,13 @@
+package com.example.kpi024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpI024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpI024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kpi024/KpI024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kpi024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpI024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
